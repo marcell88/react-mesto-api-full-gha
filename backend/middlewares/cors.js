430dca146aa8b667da731de84b32ca1e7716cd23
Markell Davydov <marcell.davydov@gmail.com>
2023-06-28 16:50:36 +0300

    server v0.6

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,20 +1,18 @@
 const cors = require('cors');
-/*
+
 const whiteList = [
   'http://localhost:3000',
 ];
-*/
+
 const corsOptions = {
   origin: (origin, callback) => {
     callback(null, true);
-    /*
     if (whiteList.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-    */
   },
 };
 
-module.exports = cors(corsOptions);
+module.exports = cors();
