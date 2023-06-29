3ef5e9b955dac851446fb81ddba1260171815920
Markell Davydov <marcell.davydov@gmail.com>
2023-06-29 13:57:50 +0300

    env path

--- backend/utils/variables.js
+++ backend/utils/variables.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: `${__dirname}../env/.env` });
 
 const { NODE_ENV = 'development', PORT = 3000, JWT_SECRET = 'super-secret' } = process.env;
 
