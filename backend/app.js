fbade085283c49be0d82922b035a902fa9d7169b
Markell Davydov <marcell.davydov@gmail.com>
2023-06-30 22:48:49 +0300

    Recs - helmet, limiter

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const defaultErrorHandler = require('./middlewares/defaultErrorHandler');
 
-const { PORT, NODE_ENV } = require('./utils/variables');
+const { PORT, NODE_ENV, DB } = require('./utils/variables');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
+mongoose.connect(DB)
   .then(() => {
     console.log('Connecteed to DB...');
   })
@@ -20,15 +29,12 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.use((req, res, next) => {
-  console.log(NODE_ENV);
-  next();
-});
-
+app.use(helmet());
+app.use(limiter);
 app.use(requestLogger);
 app.use(express.json());
 app.use('/', router);
 app.use(errorLogger);
 app.use(errors());
 app.use(defaultErrorHandler);
-app.listen(PORT, () => { console.log('Server is ON...'); });
+app.listen(PORT, () => { console.log(`Server is ON in ${NODE_ENV} mode...`); });
--- backend/utils/variables.js
+++ backend/utils/variables.js
@@ -1,10 +1,16 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../env/.env') });
 
-const { NODE_ENV = 'development', PORT = 3000, JWT_SECRET = 'super-secret' } = process.env;
+const {
+  NODE_ENV = 'development',
+  PORT = 3000,
+  JWT_SECRET = 'super-secret',
+  DB = 'mongodb://127.0.0.1:27017/mestodb',
+} = process.env;
 
 module.exports = {
   NODE_ENV,
   PORT,
   JWT_SECRET,
+  DB,
 };
